8203eab876caf410d6f68f68e16881c8cca4c3a1
adryangomesdev <adryan.gomes@arapiraca.ufal.br>
2026-08-20 08:34:46 -0300

    feat: add barra prefeitura aba sobre

--- src/main.js
+++ src/main.js
@@ -1061,6 +1061,7 @@ function renderSobre() {
         </p>
       </div>
     </section>
+    <div class="w-full h-4 md:h-6 relative z-10 bg-[url('/barra_prefeitura.png')] bg-repeat-x bg-contain bg-center"></div>
 
     <!-- Institutional Sections -->
     <section class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 space-y-16">
