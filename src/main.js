52e080918e0bd5dcb91da049bb620a2020401824
adryangomesdev <adryan.gomes@arapiraca.ufal.br>
2026-08-21 14:19:13 -0300

    feat: add barra da prefeitura noticias

--- src/main.js
+++ src/main.js
@@ -1266,6 +1266,7 @@ function renderNoticias() {
         </p>
       </div>
     </section>
+    <div class="w-full h-4 md:h-6 relative z-10 bg-[url('/barra_prefeitura.png')] bg-repeat-x bg-contain bg-center"></div>
 
     <!-- News Cards Grid -->
     <section class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
